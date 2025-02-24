8a0f61878021576949b365af31a8c5c966f2acf7
tony <antyakoub@gmail.com>
2025-02-24 12:09:02 -0500

    Added Root Route

--- src/server.js
+++ src/server.js
@@ -5,6 +5,11 @@ const youtubeRoutes = require("./routes/youtubeRoutes");
 dotenv.config();
 const app = express();
 
+
+app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+
 app.use(express.json());
 app.use("/api/youtube", youtubeRoutes);
 
